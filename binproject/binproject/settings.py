8cb2c187298bc87c06462508a2e64808ed9c3b8c
DavidEbube <davidebube@DavidEbubes-MacBook-Pro.local>
2024-05-18 14:10:06 +0100

    ...

--- binproject/binproject/settings.py
+++ binproject/binproject/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,6 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-9vz_#0md1gg21zd4bjac0wn-lwqv4d8g8qa39z5d4*xb69wk7l'
+
+# SECRET_KEY =os.getenv('SECRET_KEY')
+# DB_URL=os.getenv('DB_URL')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
