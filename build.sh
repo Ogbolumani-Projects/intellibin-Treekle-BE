d4d7f50a31d72b85045499a97f34e41f96093a6e
nireayoo <seminireayo@gmail.com>
2024-06-30 00:50:10 +0100

    build fixes

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.oy makemigrations
+python manage.py makemigrations
 python manage.py migrate
 
 python manage.py spectacular --color --file schema.yml
