fbddf28ced2b7d275e5478809b63c3a41ec17bb5
Surxezdegr8 <surces19@gmail.com>
2024-08-08 17:09:41 +0100

    updated build.sh

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ python manage.py makemigrations
 python manage.py migrate
 
 python manage.py spectacular --color --file schema.yml
-python manage.py createsuperuser --no-input 
+python manage.py createsuperuser --noinput --email "surxez19@gmail.com"
