9649bed6b2728d5ac3108daffd6c942445385ac1
Surxezdegr8 <surces19@gmail.com>
2024-08-08 17:54:15 +0100

    updated build.sh

--- authservice/models.py
+++ authservice/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from django.contrib.auth.password_validation import validate_password
 # Create your models here.
 
+
 class CustomManager(BaseUserManager):
     def create_user(self, email, password=None):
         """
@@ -29,16 +30,17 @@ class CustomManager(BaseUserManager):
         user = self.create_user(
             email,
             password=password,
+            is_superuser=True,
         )
         user.is_admin = True
         user.is_staff = True
         user.save(using=self._db)
         return user
-        
 
-class CustomUser(AbstractUser, PermissionsMixin): 
+
+class CustomUser(AbstractUser, PermissionsMixin):
     username = None
-    full_name = models.CharField(max_length= 255)
+    full_name = models.CharField(max_length=255)
     email = models.EmailField(
         verbose_name="email address",
         max_length=255,
@@ -55,15 +57,12 @@ class CustomUser(AbstractUser, PermissionsMixin):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
-    
 
     def __str__(self):
         return self.email
-    
+
 
 class UserProfile(models.Model):
     # every profile must belong one to user
     user = models.OneToOneField("CustomUser", on_delete=models.CASCADE)
     bio = models.TextField(null=True)
-
-
