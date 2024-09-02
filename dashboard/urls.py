efbb8052ffd06586088428780c18f33ba0275877
DavidEbube <davidebube@David-Ebubes-MacBook-Pro.local>
2024-09-02 17:51:09 +0100

    __

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -10,7 +10,7 @@ router.register("bin-request", WasteBinRequest, basename="bin-request")
 
 urlpatterns = [
     path("save_data/<int:pk>/", SaveBinData.as_view(), name='save_bin_data'),
-    path('api/record/', record_sensor_data, name='record_sensor_data'),
+    path('api/v1/record/', record_sensor_data, name='record_sensor_data'),
 
     
 ]+router.urls
