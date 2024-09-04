1911ad0444fb7e5e6166ac396d616de9ed83cb10
Surxezdegr8 <surces19@gmail.com>
2024-09-04 23:35:18 +0100

    updated dashboard admin.py

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import *
+from .sensor_data import SensorData
 # Register your models here.
 admin.site.register(
-    [WasteBin, WastePickUp, BinCompartment]
+    [WasteBin, WastePickUp, BinCompartment, SensorData]
 )
