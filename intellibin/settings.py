0d5e483919a7c595284fb5b8bb54efe6f2f11f05
nireayoo <seminireayo@gmail.com>
2024-09-19 18:43:27 +0100

    env changes

--- intellibin/settings.py
+++ intellibin/settings.py
@@ -107,6 +107,7 @@ DEFAULT_AUTO_FIELD ="django.db.models.BigAutoField"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+#print('DB_PASS', env('DB_PASS'))
 
 DATABASES = {
     'default': {
