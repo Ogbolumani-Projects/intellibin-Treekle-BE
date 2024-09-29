75445535ed6e8cfd94198d91d2bf4c7e14b75c77
DavidEbube <davidebube@David-Ebubes-MacBook-Pro.local>
2024-09-29 10:07:09 +0100

    s-d

--- dashboard/views.py
+++ dashboard/views.py
@@ -245,7 +245,7 @@ openapi.Parameter(
                 # weather_condition=weather_condition
             )
 
-            sensor_data = SaveSensorData.objects.all(
+            sensor_data = SaveSensorData.objects.filter(
                     'bin_id', 'waste_height', 'humidity','temperature', 
                     'weight', 'batt_value', 'latitude', 'longitude')
             data_list = list(sensor_data.values())                
