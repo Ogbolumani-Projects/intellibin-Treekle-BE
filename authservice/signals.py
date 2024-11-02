339237acd004a6b9e2d673e35bdfe1211a3c72b4
nireayoo <seminireayo@gmail.com>
2024-11-02 23:45:28 +0100

    password reset template

--- authservice/signals.py
+++ authservice/signals.py
@@ -31,7 +31,6 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
 
     # render email text
     email_html_message = render_to_string('authservice/templates/password_reset_email.html', context)
-    #email_plaintext_message = render_to_string('authservice/templates/password_reset_email.txt', context)
     email_plaintext_message = render_to_string('authservice/templates/password_reset_email.txt', context)
 
 
