58c72265547599fec4166452ab2fa33209c79f4a
davidebube <davidebube@David-Ebubes-MacBook-Pro.local>
2025-02-01 07:34:25 +0100

    changes

--- payments/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import InitiatePaymentAPIView, VerifyPaymentAPIView
-
-urlpatterns = [
-    # ... other URLs ...
-    path('payments/initiate/', InitiatePaymentAPIView.as_view(), name='initiate_payment'),
-    path('payments/verify/<str:transaction_ref>/', VerifyPaymentAPIView.as_view(), name='verify_payment'),
-]
--- waste_management/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import WasteDataAPIView
-
-urlpatterns = [
-    path('waste-data/', WasteDataAPIView.as_view(), name='waste-data'),
-]
