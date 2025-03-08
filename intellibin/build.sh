f4091dfea52ded8a951bd4e6a1fb6945bc2b0134
davidebube <davidebube@David-Ebubes-MacBook-Pro.local>
2025-03-08 15:52:07 +0100

    ..

--- intellibin/build.sh
+++ intellibin/build.sh
@@ -5,7 +5,7 @@ set -o errexit
 
 # Install dependencies
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install -r intellibin/requirements.txt
 
 # (Optional) Run database migrations (Django)
 if [ -f "manage.py" ]; then
